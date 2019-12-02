5b1e532b2252054b8e6308d8543db0ea07d9a761
KKD <kianakdonaldson@gmail.com>
2019-12-02 13:23:49 -0800

    Create sequelize functionality

--- database/index.js
+++ database/index.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize('lemonloft', 'root', 'root', {
     min: 0,
     idle: 10000
   }
-})
+});
 
 sequelize
 .authenticate()
@@ -19,6 +19,90 @@ sequelize
   console.log('Unable to connect');
 });
 
+var Activity = sequelize.define('activities', {
+  'id': {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  'title': {
+    type: Sequelize.STRING(50)
+  },
+  'image_url': {
+    type: Sequelize.STRING(300)
+  },
+  'cost': {
+    type: Sequelize.INTEGER
+  },
+  'rating': {
+    type: Sequelize.DECIMAL
+  },
+  'num_comments': {
+    type: Sequelize.INTEGER
+  },
+  'location_id': {
+    type: Sequelize.INTEGER
+  }
+});
+
+var Location = sequelize.define('locations', {
+  'id': {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  'location': {
+    type: Sequelize.STRING(50)
+  }
+});
+
+var Image = sequelize.define('images', {
+  'id': {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  'url': {
+    type: Sequelize.STRING(300)
+  }
+});
+
+var Home = sequelize.define('homes', {
+  'id': {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  'title': {
+    type: Sequelize.STRING(50)
+  },
+  'image_url': {
+    type: Sequelize.STRING(300)
+  },
+  'image_id': {
+    type: Sequelize.INTEGER
+  },
+  'cost': {
+    type: Sequelize.INTEGER
+  },
+  'rating': {
+    type: Sequelize.DECIMAL
+  },
+  'num_comments': {
+    type: Sequelize.INTEGER
+  },
+  'location_id': {
+    type: Sequelize.INTEGER
+  },
+  'room_type': {
+    type: Sequelize.STRING(50)
+  }
+});
+
 module.exports = {
-  sequelize
+  sequelize,
+  Activity,
+  Location,
+  Image,
+  Home
 }
