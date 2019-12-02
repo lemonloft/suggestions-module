65b30cd765d1779739447d2bda7e4fc5490d6b36
KKD <kianakdonaldson@gmail.com>
2019-12-01 19:16:45 -0800

    Initialize repo with scripts running for webpack and nodemon
    Create folders/files and implement server and client connections

---
+++ client/src/Components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ajax } from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default App;
