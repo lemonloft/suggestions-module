854e1b2d58d1c309808314d93079691819b01bc6
KKD <kianakdonaldson@gmail.com>
2019-12-02 22:26:38 -0800

    Create GET request of json data

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const port = 3000;
 const morgan = require('morgan');
+const { Activity, Listing } = require('../database/index.js');
 
 app.use('/', express.static(path.join(__dirname, '../client/dist'))); // learn more
 
@@ -11,9 +12,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get('/suggestions/activities', (req, res) => {
+  Activity.findAll().then(activities => res.json(activities));
 });
+
+// app.get('/suggestions/listings', (req, res) => {
+//   Listing.findAll().then(listings => res.json(listings));
+// });
 
 app.listen(port, () => {
   console.log(`We be listening on port ${port}`);
