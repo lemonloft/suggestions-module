5065c603e9972c96d3da0d889ce8afc26a7089e7
KKD <kianakdonaldson@gmail.com>
2019-12-08 20:22:23 -0800

    Refactor seeding script

--- database/seedLocation.js
+++ database/seedLocation.js
@@ -1,4 +1,4 @@
-const faker = require('faker');
+// const faker = require('faker');
 
 // const seedLocation = () => {
 //   for (let i = 0; i <= 10; i++) {
@@ -20,26 +20,26 @@ const faker = require('faker');
 //   }
 // }
 
-var seedLocation = () => {
-  let locations = {};
-  let location;
+// var seedLocation = () => {
+//   let locations = {};
+//   let location;
   
-  for (var i = 0; i <= 100; i++) {
+//   for (var i = 0; i <= 100; i++) {
 
-    if (i === 0) {
-      location = faker.address.city();
-      locations[i] === location;
-    } else if (i !== 0 && i < 10) {
-      locations[i] = locations[i - 1];
-    } else if (i % 10 === 0) {
-      location = faker.address.city();
-      locations[i] = location;
-    } else {
-      locations[i] = locations[i - 1];
-    }
-  }
+//     if (i === 0) {
+//       location = faker.address.city();
+//       locations[i] === location;
+//     } else if (i !== 0 && i < 10) {
+//       locations[i] = locations[i - 1];
+//     } else if (i % 10 === 0) {
+//       location = faker.address.city();
+//       locations[i] = location;
+//     } else {
+//       locations[i] = locations[i - 1];
+//     }
+//   }
 
-  return locations;
-}
+//   return locations;
+// }
 
-seedLocation();
+// seedLocation();
