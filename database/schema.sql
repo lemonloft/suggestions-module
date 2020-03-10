03303b0486a0e34bfed39465f7988114bbbe59c5
KKD <kianakdonaldson@gmail.com>
2020-03-10 11:28:12 -0700

    Refactor schema

--- database/schema.sql
+++ database/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE suggestions;
 
 USE suggestions;
 
-DROP TABLE IF EXISTS `images`;
-CREATE TABLE `images` (
-  `image_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `url` VARCHAR(300),
-  CONSTRAINT image_id PRIMARY KEY (image_id)
-)
+-- DROP TABLE IF EXISTS `images`;
+-- CREATE TABLE `images` (
+--   `image_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   `url` VARCHAR(300),
+--   CONSTRAINT image_id PRIMARY KEY (image_id)
+-- )
 
 DROP TABLE IF EXISTS `activities`;
 CREATE TABLE `activities` (
@@ -35,8 +35,8 @@ CREATE TABLE `listings` (
   `num_comments` INT,
   -- `location_id` INT,
   `location` VARCHAR(100),
-  `room_type` VARCHAR(100),
-  FOREIGN KEY (image_id) REFERENCES images (image_id)
+  `room_type` VARCHAR(100)
+  -- FOREIGN KEY (image_id) REFERENCES images (image_id)
   -- FOREIGN KEY (location_id) REFERENCES locations (id) ON DELETE CASCADE
 )
 
